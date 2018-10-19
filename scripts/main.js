2324218a3daf131fea19546a5f0156389a3a9bd4
gitprzemek <przemyslaw.sz.home@gmail.com>
2018-10-19 17:33:49 +0200

    add buttons to movie containers

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,11 @@ let movieList = new XMLHttpRequest();
 let movieObj;
 let movieResult;
 
-const poster = document.getElementsByClassName("movie-container__img");
-const rating = document.getElementsByClassName("movie-container__percent");
-const title = document.getElementsByClassName("movie-container__title");
-const dateMovie = document.getElementsByClassName("movie-container__date");
-const about = document.getElementsByClassName("movie-container__text");
+// const poster = document.getElementsByClassName("movie-container__img");
+// const rating = document.getElementsByClassName("movie-container__percent");
+// const title = document.getElementsByClassName("movie-container__title");
+// const dateMovie = document.getElementsByClassName("movie-container__date");
+// const about = document.getElementsByClassName("movie-container__text");
 
 movieList.open("GET", "https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=642874b006093ef1d8becb7a5a90179c", true);
 movieList.responseType = "text";
@@ -17,19 +17,8 @@ movieList.send(null);
 
 
 movieList.onload = function(){
-    // class movieTemp {
-    //     constructor(id, original_title){
-    //         let overview, poster_path, release_date, title, vote_average;
-    //         this.id = id;
-    //         this.original_title = original_title;
-    //         this.overview = overview;
-    //         this.poster_path = poster_path;
-    //         this.release_date = release_date;
-    //         this.title = title;
-    //         this.vote_average = vote_average;
-    
-    //     }
-    // }
+
+    // CARD TEMPLATE HTML
     function movieCard(movie){
         return `
         <div class="movie-container">
@@ -42,7 +31,6 @@ movieList.onload = function(){
                 <span class="movie-container__star"><input type="checkbox" class="ss"></span>
             </div>
         </div>
-        
         `
     }
     if (movieList.status === 200){
@@ -52,10 +40,11 @@ movieList.onload = function(){
         console.log(movieObj);
         console.log(movieResult);
     }
+
     // MAX LENGHT TEXT IN CONTAINER ABOUT
-    let i;
+    
     const maxText = document.getElementsByClassName("movie-container__text");
-    for (i = 0; i < maxText.length; i++) {
+    for (let i = 0; i < maxText.length; i++) {
         if (maxText[i].className == "movie-container__text") {
             maxText[i].innerHTML = maxText[i].innerHTML.substring(0, 210) + '<span>...</span>';
         }
