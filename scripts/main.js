7782f1368fcc2f6c5e8ae1c1447cf8cb97d4e0b5
gitprzemek <przemyslaw.sz.home@gmail.com>
2018-10-19 22:27:39 +0200

    add search code

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
-'use strict'
+
 
 let movieList = new XMLHttpRequest();
+let movieSearch = new XMLHttpRequest();
 let movieObj;
 let movieResult;
+let searchObj;
+let searchResult;
+
 
 // const poster = document.getElementsByClassName("movie-container__img");
 // const rating = document.getElementsByClassName("movie-container__percent");
@@ -13,44 +17,75 @@ let movieResult;
 movieList.open("GET", "https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=642874b006093ef1d8becb7a5a90179c", true);
 movieList.responseType = "text";
 movieList.send(null);
+console.log(movieList.status);
 
-
+// CARD TEMPLATE HTML
+function movieCard(movie){
+    return `
+    <div class="movie-container">
+        <img src="http://image.tmdb.org/t/p/w185/${movie.poster_path}" alt="NO PHOTO" class="movie-container__img">
+        <div class="movie-container__about">
+            <span class="movie-container__percent">${movie.vote_average}</span>
+            <h2 class="movie-container__title">${movie.original_title}</h2>
+            <p class="movie-container__date">${movie.release_date}</p>
+            <p class="movie-container__text">${movie.overview}</p>
+            <span class="movie-container__star"><input type="checkbox" class="ss"></span>
+        </div>
+    </div>
+    `
+}
+// MAX LENGHT TEXT IN CONTAINER ABOUT
+function shortText(){
+    const maxText = document.getElementsByClassName("movie-container__text");
+    for (let i = 0; i < maxText.length; i++) {
+        if (maxText[i].className == "movie-container__text") {
+            maxText[i].innerHTML = maxText[i].innerHTML.substring(0, 210) + '<span>...</span>';
+        }
+    } 
+}
 
 movieList.onload = function(){
-
-    // CARD TEMPLATE HTML
-    function movieCard(movie){
-        return `
-        <div class="movie-container">
-            <img src="http://image.tmdb.org/t/p/w185/${movie.poster_path}" alt="movie-image" class="movie-container__img">
-            <div class="movie-container__about">
-                <span class="movie-container__percent">${movie.vote_average}</span>
-                <h2 class="movie-container__title">${movie.original_title}</h2>
-                <p class="movie-container__date">${movie.release_date}</p>
-                <p class="movie-container__text" id="xxx">${movie.overview}</p>
-                <span class="movie-container__star"><input type="checkbox" class="ss"></span>
-            </div>
-        </div>
-        `
-    }
     if (movieList.status === 200){
+        console.log(movieList.status);
         movieObj = JSON.parse(movieList.responseText);
         movieResult = movieObj.results;
         document.getElementById("movie-section").innerHTML = `${movieResult.map(movieCard).join("")}`;
         console.log(movieObj);
         console.log(movieResult);
     }
-
-    // MAX LENGHT TEXT IN CONTAINER ABOUT
-    
-    const maxText = document.getElementsByClassName("movie-container__text");
-    for (let i = 0; i < maxText.length; i++) {
-        if (maxText[i].className == "movie-container__text") {
-            maxText[i].innerHTML = maxText[i].innerHTML.substring(0, 210) + '<span>...</span>';
-        }
-    }
+    shortText(); 
 }
+// NEW REQUEST FOR SEARCH AFTER CLICK BUTTON
 
+
+const btnSearch = document.getElementById("search-form__btn");
+btnSearch.addEventListener("click", function(){
+    // REQ FOR SEARCH
+    let inputSearch = document.getElementById("search-form__input").value;
+    movieSearch.open("GET", "https://api.themoviedb.org/3/search/movie?api_key=642874b006093ef1d8becb7a5a90179c&query="+inputSearch+'"', true);
+    movieSearch.responseType = "text";
+    movieSearch.onload = function(){
+        if (movieSearch.status === 200){
+            console.log(movieSearch.status);
+            searchObj = JSON.parse(movieSearch.responseText);
+            searchResult = searchObj.results;
+            console.log(inputSearch)
+            document.getElementById("movie-section").innerHTML = `${searchResult.map(movieCard).join("")}`;
+            console.log(searchObj);
+            console.log(searchResult);
+        }
+        shortText();
+    }
+    movieSearch.send(null);
+
+})
+
+const btnNext = document.getElementById("nextBtn");
+btnNext.addEventListener("click", function(page){
+    for(let i = 0; i < page.total_pages; i++){
+        return page.page[i];
+    }
+})
 
 
 
